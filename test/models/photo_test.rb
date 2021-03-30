af6c574bebcb575a1c4abf94eb6f3ac8d46f7747
rmzNadir <dgzrz99@gmail.com>
2021-03-30 17:16:36 -0600

    photos tests

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -8,10 +8,26 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-require "test_helper"
+require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should have a title value' do
+    photo = Photo.new(image_url: 'algo.jpg')
+    assert !photo.save
+  end
+
+  test 'should have an image_url value' do
+    photo = Photo.new(title: 'algo')
+    assert !photo.save
+  end
+
+  test 'should not save non valid image urls' do
+    photo = Photo.new(title: 'algo', image_url: 'algo')
+    assert !photo.save
+  end
+
+  test 'should save valid image urls' do
+    photo = Photo.new(title: 'algo', image_url: 'algo.jpg')
+    assert photo.save
+  end
 end
