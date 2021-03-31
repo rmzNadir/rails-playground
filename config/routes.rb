5bd9f97f40bbac4ec7ad3f52348cdf75216998bb
rmzNadir <dgzrz99@gmail.com>
2021-03-31 17:39:55 -0600

    :construction: nested resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   resources :tweets
   #  Cambia la url base de /photos a /fotos, manteniendo el resto en el modelo photos y controlador photos
-  resources :fotos, as: :photos, controller: :photos
+  resources :fotos, as: :photos, controller: :photos, path_names: { new: 'nuevo', edit: 'editar' } do
+    member do
+      # /fotos/:id/preview
+      get 'preview'
+    end
+
+    collection do
+      # /fotos/admin
+      get 'admin'
+    end
+  end
 
   root to: 'main#hello'
 
