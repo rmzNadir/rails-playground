e7f276425ea12ce95fb05d2c9038c27c597862c6
rmzNadir <dgzrz99@gmail.com>
2021-03-31 10:50:19 -0600

    create / show new records

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,22 @@
 class PhotosController < ApplicationController
-  def index; end
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    photo = Photo.new
+
+    photo.title = params[:photo][:title]
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+
+    redirect_to photo
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :tweets
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :photos
+
   root to: 'main#hello'
-  get 'photos/index'
 
   get 'saludo', to: 'main#hi'
   post 'hi', to: 'main#greetings'
