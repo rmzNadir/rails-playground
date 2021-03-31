844e8de74f4e5f36d122dac0f5bc630103aa8a59
rmzNadir <dgzrz99@gmail.com>
2021-03-31 11:06:24 -0600

    Edit records

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,4 +19,20 @@ class PhotosController < ApplicationController
 
     redirect_to photo
   end
+
+  # /photos/:id/edit
+  def edit
+    @id = params[:id]
+  end
+
+  def update
+    photo = Photo.find(params[:id])
+
+    photo.title = params[:photo][:title]
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+
+    redirect_to photo
+  end
 end
