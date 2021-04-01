3f6a8f05ada406b1898b9b53951c4a65b02aa431
rmzNadir <dgzrz99@gmail.com>
2021-04-01 17:17:12 -0600

    Pagination

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@
 #
 # Table name: photos
 #
-#  id         :integer          not null, primary key
+#  id         :integer          not null, primary key, has index for faster ordering
 #  title      :string
 #  image_url  :string
 #  created_at :datetime         not null
@@ -11,4 +11,8 @@
 class Photo < ApplicationRecord
   validates :title, presence: true
   validates :image_url, presence: true, format: { with: /.\.(png|jpeg|jpg|gif)/ }
+
+  def self.paginate(page = 1, per_page = 15)
+    Photo.order('id desc').offset((page - 1) * per_page).limit(per_page)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   resources :tweets
   #  Cambia la url base de /photos a /fotos, manteniendo el resto en el modelo photos y controlador photos
-  resources :fotos, as: :photos, controller: :photos, path_names: { new: 'nuevo', edit: 'editar' } do
-    member do
-      # /fotos/:id/preview
-      get 'preview'
-    end
+  resources :fotos, as: :photos, controller: :photos, path_names: { new: 'nuevo', edit: 'editar' } # do
+  #   member do
+  #     # /fotos/:id/preview
+  #     get 'preview'
+  #   end
 
-    collection do
-      # /fotos/admin
-      get 'admin'
-    end
-  end
+  #   collection do
+  #     # /fotos/admin
+  #     get 'admin'
+  #   end
+  # end
 
   root to: 'main#hello'
 
